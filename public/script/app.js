0bf673a968ab8bd537f96bacd5b7c3efeee49846
Spencer Strickland <sstricklandcs@gmail.com>
2023-11-02 23:02:44 -0400

    new for a2

---
+++ public/script/app.js
@@ -0,0 +1,11 @@
+// Immidiate invoked function express
+
+(function(){
+    function Start({
+        console.log("application started....")
+
+    })
+
+    window.addEventListiner("load", Start)
+
+})
